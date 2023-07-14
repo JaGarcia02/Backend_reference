c8739fb112ae51ffa268c99a274795a760395b08
JaGarcia02 <jagarcia02051998@gmail.com>
2023-07-14 15:37:58 +0800

    update commit

--- node_jwt_cookie_backend/array_api/controllers/recipe_controller.js
+++ node_jwt_cookie_backend/array_api/controllers/recipe_controller.js
@@ -13,18 +13,74 @@ const Create_Dish = async (req, res) => {
 const Add_Recipe = async (req, res) => {
   try {
     const { id } = req.params;
-    const { recipe } = req.body;
+    const { recipe, recipe_id } = req.body;
 
     const dish_data = await Recipe.findById({ _id: id });
+
+    // if (!dish_data) {
+    //   return res.status(401).json({ message: "Dish not found!" });
+    // } else {
+    //   dish_data.recipe.push(recipe_id, recipe);
+    // }
+    // await dish_data.save();
+
+    // const recipeList = await Promise.all(
+    //   dish_data.recipe.map((id) => Recipe.findById(id))
+    // );
+
+    // const formatedRecipeList = recipeList.map(({ recipe_id, recipe }) => {
+    //   return { recipe_id, recipe };
+    // });
+    // return res.status(200).json(formatedRecipeList);
+
+    // if (!dish_data) {
+    //   return res.status(401).json({ message: "Dish not found!" });
+    // }
+    // await Recipe.findByIdAndUpdate(
+    //   { _id: id },
+    //   { recipe: { recipe: recipe, recipe_id: recipe_id } }
+    // );
+    // const recipe_list = await Recipe.findById({ _id: id });
+
     if (!dish_data) {
       return res.status(401).json({ message: "Dish not found!" });
+    } else {
+      dish_data.recipe.push({ recipe_id: recipe_id, recipe: recipe });
     }
-    await Recipe.findByIdAndUpdate({ _id: id }, { recipe: recipe });
-    const recipe_list = await Recipe.findById({ _id: id });
+    await dish_data.save();
 
-    res.status(200).json(recipe_list);
+    return res.status(200).json(dish_data.recipe);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-    return console.log(recipe_list.recipe);
+const Delete_Recipe = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { recipe_id } = req.body;
+
+    const dish_data = await Recipe.findById({ _id: id });
+
+    // if (
+    //   dish_data.recipe.filter((fil) => fil.recipe_id == recipe_id)[0]
+    //     .recipe_id == recipe_id
+    // ) {
+
+    //   // return res.status(200).json({
+    //   //   message: "This is the data!",
+    //   //   payload: dish_data.recipe.filter((fil) => fil.recipe_id == recipe_id),
+    //   // });
+    // } else {
+    //   console.log("error!");
+    // }
+
+    const newArray = dish_data.recipe.filter(
+      (fil) => fil.recipe_id !== recipe_id
+    );
+
+    const test = await Recipe.updateOne({ recipe: newArray });
+    return res.status(200).json(test);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
@@ -39,12 +95,42 @@ const Get_Dish = async (req, res) => {
   }
 };
 
-module.exports = { Create_Dish, Add_Recipe, Get_Dish };
+module.exports = { Create_Dish, Add_Recipe, Get_Dish, Delete_Recipe };
 
 /*
 
     In this concept you need for just to add multiple array in the database,
     this will be abble to save muliple object in the frontend.
 
+    const Delete_Recipe = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { recipe_id } = req.body;
+
+    const dish_data = await Recipe.findById({ _id: id });
+
+    // if (
+    //   dish_data.recipe.filter((fil) => fil.recipe_id == recipe_id)[0]
+    //     .recipe_id == recipe_id
+    // ) {
+    //   console.log("this is the data");
+    //   return res.status(200).json({
+    //     message: "This is the data!",
+    //     payload: dish_data.recipe.filter((fil) => fil.recipe_id == recipe_id),
+    //   });
+    // } else {
+    //   console.log("error!");
+    // }
+    const test = await Recipe.updateOne({ recipe: recipe_id });
+
+    return res.status(200).json(
+      // dish_data.recipe.filter((fil) => fil.recipe_id == recipe_id)[0]
+      //   .recipe_id
+      test
+    );
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 */
--- node_jwt_cookie_backend/array_api/routes/recipe_routes.js
+++ node_jwt_cookie_backend/array_api/routes/recipe_routes.js
@@ -4,9 +4,11 @@ const {
   Create_Dish,
   Add_Recipe,
   Get_Dish,
+  Delete_Recipe,
 } = require("../controllers/recipe_controller");
 
 router.post("/add-dish", Create_Dish);
+router.put("/delete-dish/:id", Delete_Recipe);
 router.put("/add-recipe/:id", Add_Recipe);
 router.get("/view-recipe", Get_Dish);
 
