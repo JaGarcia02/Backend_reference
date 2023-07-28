3e709a5d29532dbc9b8cb772da57b3085820f73b
JaGarcia02 <jagarcia02051998@gmail.com>
2023-07-28 17:30:08 +0800

    update commit

--- bulkcreate_node/bulk_create_01/controller/timekeeping_controller.js
+++ bulkcreate_node/bulk_create_01/controller/timekeeping_controller.js
@@ -5,6 +5,8 @@ const save_cutoff = async (req, res) => {
     const { timekeeping, dtr } = req.body;
     const new_data = await time_keeping.bulkCreate(timekeeping);
     await time_dtr.bulkCreate(dtr);
+    let test;
+    console.log({ test: { new_data: new_data, time_dtr: dtr } });
     return res.status(200).json(new_data);
   } catch (error) {
     res.status(500);
@@ -16,10 +18,9 @@ const view_data2 = async (req, res) => {
   try {
     const dtr = await time_dtr.findAll();
     const summary = await time_keeping.findAll();
+    const obj = { data: { summary: summary, dtr: dtr } };
 
-    const timekeeping_data = { data: { summary: summary, dtr: dtr } };
-
-    return res.status(200).json(timekeeping_data);
+    return res.status(200).json(obj);
   } catch (error) {
     res.status(500);
     throw new Error(error);
