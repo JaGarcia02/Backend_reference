096f375c5c5128fa2dba7ed6f8593ecc9b30a5cf
Ja Garcia <jagarcia02051998@gmail.com>
2023-08-29 23:31:24 +0800

    prisma tutorial

--- node_prisma/server/server.js
+++ node_prisma/server/server.js
@@ -6,38 +6,84 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/user", async (req, res) => {
   const allUsers = await prisma.user.findMany();
   return res.status(200).json(allUsers);
 });
-app.post("/", async (req, res) => {
-  const newUser = await prisma.user.create({ data: req.body });
+app.post("/user", async (req, res) => {
+  const newUser = await prisma.user.create({
+    data: req.body,
+  });
   return res.status(200).json(newUser);
 });
-app.put("/:id", async (req, res) => {
+app.put("/user/:id", async (req, res) => {
   const { id } = req.params;
   const newAge = req.body.age;
   const updateUser = await prisma.user.update({
-    where: { id: parseInt(id) },
+    where: { id: id },
     data: { age: newAge },
   });
   return res.status(200).json(updateUser);
 });
-app.delete("/:id", async (req, res) => {
+app.delete("/user/:id", async (req, res) => {
   const { id } = req.params;
   const deletedUser = await prisma.user.delete({
-    where: { id: parseInt(id) },
+    where: { id: id },
   });
   return res
     .status(200)
     .json({ message: "User Deleted!", response: deletedUser });
 });
-app.get("/:id", async (req, res) => {
-  const { id } = req.params;
+app.get("/user/:id", async (req, res) => {
+  const id = req.params.id;
   const userProfile = await prisma.user.findUnique({
-    where: { id: parseInt(id) },
+    where: { id: id },
   });
   return res.status(200).json(userProfile);
+});
+app.post("/house", async (req, res) => {
+  const newHouse = await prisma.house.create({ data: req.body });
+  return res.status(200).json(newHouse);
+});
+app.post("/house/many", async (req, res) => {
+  const newHouse = await prisma.house.createMany({ data: req.body });
+  return res.status(200).json(newHouse);
+});
+app.get("/house", async (req, res) => {
+  const allHouse = await prisma.house.findMany({
+    include: { owner: true, builtBy: true },
+  });
+  return res.status(200).json(allHouse);
+});
+app.get("/house", async (req, res) => {
+  const address = req.body.address;
+  const allHouse = await prisma.house.findUnique({
+    where: { address: address },
+    include: { owner: true, builtBy: true },
+  });
+  return res.status(200).json(allHouse);
+});
+app.get("/house/filtered", async (req, res) => {
+  const address = req.body.address;
+  const allHouse = await prisma.house.findMany({
+    where: {
+      wifiProvider: { not: null },
+      owner: {
+        age: {
+          gte: 22,
+        },
+      },
+    },
+    orderBy: [
+      {
+        owner: {
+          firstName: "desc",
+        },
+      },
+    ],
+    include: { owner: true, builtBy: true },
+  });
+  return res.status(200).json(allHouse);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
