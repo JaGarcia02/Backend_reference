468d0c528927b9e2675f1b0ad0f14cc51ec0d9ce
JaGarcia02 <jagarcia02051998@gmail.com>
2023-09-11 16:25:44 +0800

    new prototype timekeeping

--- bulkcreate_node/bulk_create_02/controllers/New_DTR_controller.js
+++ bulkcreate_node/bulk_create_02/controllers/New_DTR_controller.js
@@ -2,15 +2,8 @@ const { new_DTR } = require("../models");
 
 const dtr_submit = async (req, res) => {
   try {
-    const { cutoff, time_in, break_out, break_in, time_out, bioId } = req.body;
-    await new_DTR.create({
-      Cutoff: cutoff,
-      Time_in: time_in,
-      Time_break_out: break_out,
-      Time_break_in: break_in,
-      Time_out: time_out,
-      BioID: bioId,
-    });
+    const { dtr } = req.body;
+    await new_DTR.bulkCreate(dtr);
     const created_data = await new_DTR.findAll({});
     return res.status(200).json(created_data);
   } catch (error) {
